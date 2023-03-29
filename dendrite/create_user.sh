7af73aeb169a2b1f62b5ec57b94354a497b1a1ab
c-jaenicke <me@chrisj.de>
2023-03-29 11:22:02 +0200

    Add info to dendrite

--- dendrite/create_user.sh
+++ dendrite/create_user.sh
@@ -3,5 +3,6 @@
 # Using first argument as username
 # add -admin to add admin account
 # https://matrix-org.github.io/dendrite/administration/createusers
+# HAVE TO SET A SHARED REGISTRATION SECRET IN CONFIG TO USE THIS
 
 docker exec -it dendrite-monolith /usr/bin/create-account -config /etc/dendrite/config/dendrite.yaml -username $1
